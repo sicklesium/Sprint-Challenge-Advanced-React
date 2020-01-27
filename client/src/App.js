e4c319fae1c8de445395329345fc15fb3c2ffed2
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-01-26 21:50:57 -0500

    Added custom hook - dark mode and local storage hook.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import PlayerCard from "./components/PlayerCard.js"
 import NavBar from "./components/NavBar.js"
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import UseDarkMode from '../hooks/UseDarkMode.js';
 
 const NavBar = () => {
+    const [darkMode, setDarkMode] = UseDarkMode(false);
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+
     return (
         <nav className="navbar">
             <h1>Women's World Cup Players</h1>
+            <div className="dark-mode__toggle">
+                <div
+                    onClick={toggleMode}
+                    className={darkMode ? 'toggle toggled' : 'toggle'}
+                />
+            </div>
             <h3>June-July, 2019, Worldwide</h3>
+
         </nav >
     );
 };
